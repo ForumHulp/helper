30f17ecbab17c45bda55129b8c67c998f58f9cbc
Forumhulp.com <info@forumhulp.com>
2016-02-21 09:02:51 +0100

    3.1.7

--- helper.php
+++ helper.php
@@ -9,8 +9,11 @@
 
 namespace forumhulp\helper;
 
+use \phpbb\filesystem\filesystem;
+
 class helper 
 {
+	protected $db;
 	protected $config;
 	protected $phpbb_extension_manager;
 	protected $template;
@@ -21,6 +24,7 @@ class helper
 	protected $root_path;
 
 	public function __construct(
+		\phpbb\db\driver\driver_interface $db,
 		\phpbb\config\config $config,
 		\phpbb\extension\manager $phpbb_extension_manager,
 		\phpbb\template\template $template,
@@ -30,6 +34,7 @@ class helper
 		\phpbb\cache\service $cache,
 		$root_path)
 	{
+		$this->db = $db;
 		$this->config = $config;
 		$this->phpbb_extension_manager = $phpbb_extension_manager;
 		$this->template = $template;
@@ -166,6 +171,7 @@ class helper
 		$files_changed = array();
 		foreach($files as $key => $file)
 		{
+			phpbb_chmod($this->root_path . $file, CHMOD_WRITE);
 			if (is_writable($this->root_path . $file))
 			{
 				$fp = @fopen($this->root_path . $file , 'r' );
@@ -211,5 +217,83 @@ class helper
 			$not_updated = array_diff($files, $files_changed);
 			$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, (($revert) ? 'LOG_CORE_NOT_REPLACED' : 'LOG_CORE_NOT_UPDATED'), time(), array(implode('<br />', $not_updated)));
 		}
+	}
+
+	/**
+	* Move module position by $steps up/down, 3.1 compatabillity
+	*/
+	function move_module_by($module_row, $module_class, $action = 'move_up', $steps = 1)
+	{
+		/**
+		* Fetch all the siblings between the module's current spot
+		* and where we want to move it to. If there are less than $steps
+		* siblings between the current spot and the target then the
+		* module will move as far as possible
+		*/
+		$sql = 'SELECT module_id, left_id, right_id, module_langname
+			FROM ' . MODULES_TABLE . "
+			WHERE module_class = '" . $this->db->sql_escape($module_class) . "'
+				AND parent_id = " . (int) $module_row['parent_id'] . '
+				AND ' . (($action == 'move_up') ? 'right_id < ' . (int) $module_row['right_id'] . ' 
+				ORDER BY right_id DESC' : 'left_id > ' . (int) $module_row['left_id'] . ' ORDER BY left_id ASC');
+		$result = $this->db->sql_query_limit($sql, $steps);
+
+		$target = array();
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$target = $row;
+		}
+		$this->db->sql_freeresult($result);
+
+		if (!sizeof($target))
+		{
+			// The module is already on top or bottom
+			return false;
+		}
+
+		/**
+		* $left_id and $right_id define the scope of the nodes that are affected by the move.
+		* $diff_up and $diff_down are the values to substract or add to each node's left_id
+		* and right_id in order to move them up or down.
+		* $move_up_left and $move_up_right define the scope of the nodes that are moving
+		* up. Other nodes in the scope of ($left_id, $right_id) are considered to move down.
+		*/
+		if ($action == 'move_up')
+		{
+			$left_id = (int) $target['left_id'];
+			$right_id = (int) $module_row['right_id'];
+
+			$diff_up = (int) ($module_row['left_id'] - $target['left_id']);
+			$diff_down = (int) ($module_row['right_id'] + 1 - $module_row['left_id']);
+
+			$move_up_left = (int) $module_row['left_id'];
+			$move_up_right = (int) $module_row['right_id'];
+		}
+		else
+		{
+			$left_id = (int) $module_row['left_id'];
+			$right_id = (int) $target['right_id'];
+
+			$diff_up = (int) ($module_row['right_id'] + 1 - $module_row['left_id']);
+			$diff_down = (int) ($target['right_id'] - $module_row['right_id']);
+
+			$move_up_left = (int) ($module_row['right_id'] + 1);
+			$move_up_right = (int) $target['right_id'];
+		}
+
+		// Now do the dirty job
+		$sql = 'UPDATE ' . MODULES_TABLE . "
+			SET left_id = left_id + CASE
+				WHEN left_id BETWEEN {$move_up_left} AND {$move_up_right} THEN -{$diff_up}
+				ELSE {$diff_down}
+			END,
+			right_id = right_id + CASE
+				WHEN right_id BETWEEN {$move_up_left} AND {$move_up_right} THEN -{$diff_up}
+				ELSE {$diff_down}
+			END
+			WHERE module_class = '" . $this->db->sql_escape($module_class) . "'
+				AND left_id BETWEEN {$left_id} AND {$right_id}
+				AND right_id BETWEEN {$left_id} AND {$right_id}";
+		$this->db->sql_query($sql);
 	}
 }
