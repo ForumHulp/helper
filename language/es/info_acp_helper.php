185f4808e1fcaf5bbe9e9fd8786efa3bb9dbcab1
Raphaël M <Galixte@users.noreply.github.com>
2017-07-23 03:21:29 +0200

    Adds a new language key to disable all extensions button

--- language/es/info_acp_helper.php
+++ language/es/info_acp_helper.php
@@ -18,6 +18,8 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
+	'FH_DISABLE_ALL'		=> 'Disable all extensions',
+
 	'LOG_CORE_INSTALLED'	=> '<strong>Forumhulp Helper</strong><br />» Archivos cambiados correctamente',
 	'LOG_CORE_DEINSTALLED'	=> '<strong>Forumhulp Helper</strong><br />» Archivos restaurados correctamente',
 	'LOG_CORE_NOT_REPLACED'	=> '<strong>Forumhulp Helper</strong><br />» No se pudo reemplazar el/los archivo(s)<br />» %s',
